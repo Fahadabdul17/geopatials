7d35df2e2135e9e6339f8393090f3d8dcb056a0a
fahad17 <fahadra96@gmail.com>
2024-02-02 08:48:28 +0700

    Update coordinates in test functions

--- fahad_test.go
+++ fahad_test.go
@@ -24,11 +24,11 @@ func TestGeoWithin(t *testing.T) {
 	coordinates := Polygon{
 		Coordinates: [][][]float64{
 			{
-				{106.83515985012605, -6.166125656657883},
-				{106.83582623098323, -6.167892388658771},
-				{106.83893600831055, -6.1663096915163464},
-				{106.83834366977129, -6.1641380761234075},
-				{106.83515985012605, -6.166125656657883},
+				{107.90241395501647, -7.2159039954805735},
+				{107.90241973415237, -7.216093196465536},
+				{107.90275492403225, -7.216093196465536},
+				{107.90275492403225, -7.2159039954805735},
+				{107.90241395501647, -7.2159039954805735},
 			},
 		},
 	}
@@ -40,7 +40,7 @@ func TestNear(t *testing.T) {
 	mconn := SetConnection2dsphere("MONGOSTRING", "gis", "gis")
 	coordinates := Point{
 		Coordinates: []float64{
-			106.80899259801674, -6.168340926357345,
+			107.90230814756461, -7.212792294268141,
 		},
 	}
 	datagedung := Near(mconn, "gis", coordinates)
@@ -51,7 +51,7 @@ func TestNearSphere(t *testing.T) {
 	mconn := SetConnection("MONGOSTRING", "gis")
 	coordinates := Point{
 		Coordinates: []float64{
-			106.81781953215608, -6.167583300161709,
+			107.90357575285924, -7.213350872053013,
 		},
 	}
 	datagedung := NearSphere(mconn, "gis", coordinates)
@@ -62,8 +62,8 @@ func TestBox(t *testing.T) {
 	mconn := SetConnection("MONGOSTRING", "gis")
 	coordinates := Polyline{
 		Coordinates: [][]float64{
-			{106.8200974960248, -6.163418436577771},
-			{106.82086127204684, -6.167848024969331},
+			{107.90212716477299, -7.213924382944427},
+			{107.90395765245472, -7.213931661492197},
 		},
 	}
 	datagedung := Box(mconn, collname, coordinates)
